be96f798ef74341d3b388ab0c94385ad88a960d5
elenanope <elenaromanmoreno@gmail.com>
2025-03-08 18:23:22 +0100

    CommitDeClase

--- The Sheperd's Way/Assets/TheShepersWay_Root/Scripts/DogController.cs
+++ The Sheperd's Way/Assets/TheShepersWay_Root/Scripts/DogController.cs
@@ -6,23 +6,55 @@ using UnityEngine.InputSystem;
 public class DogController : MonoBehaviour
 {
     [SerializeField] float dogSpeed;
+    public bool isFainted;
+    [SerializeField] bool canBark1;
     Vector2 moveInput;
-    Rigidbody2D rb;
+    Rigidbody2D dogRb;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        dogRb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
         Move();
     }
+    public float sheepPushDistance = 5f; // How far the object will move toward the hit point
+
+    void Update()
+    {
+        if (canBark1)
+        {
+            //Arreglar todo
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right);
+            Debug.DrawRay(transform.position, transform.right * 30f, Color.red);
+            canBark1 = false;
+            // Verifica si el objeto golpeado tiene el tag especificado
+            if (hit.collider.CompareTag("Sheep"))
+                {
+                    Vector2 direction = (hit.point - (Vector2)transform.position).normalized;
+                    // Si el objeto golpeado tiene un Rigidbody2D, mueve el objeto con su Rigidbody2D
+                    Rigidbody2D sheepRb = hit.collider.GetComponent<Rigidbody2D>();
+                    if (sheepRb != null)
+                    {
+                        // Mueve el objeto "Sheep" usando su Rigidbody2D
+                        sheepRb.velocity = direction * sheepPushDistance;
+                    }
+                    else
+                    {
+                        Debug.Log("No hay rigidbody");
+                    }
+
+                }
+            
+        }
+    }
 
     void Move()
     {
-        rb.velocity = moveInput * dogSpeed;
+        dogRb.velocity = moveInput * dogSpeed;
     }
 
 
@@ -35,7 +67,11 @@ public class DogController : MonoBehaviour
 
     public void OnBark1(InputAction.CallbackContext context)
     {
-
+        if(context.performed)
+        {
+            canBark1 = true;
+            Debug.Log("You barked!");
+        }
     }
     public void OnBark2(InputAction.CallbackContext context)
     {
--- The Sheperd's Way/Assets/TheShepersWay_Root/Scripts/Managers/GameManager.cs
+++ The Sheperd's Way/Assets/TheShepersWay_Root/Scripts/Managers/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        instance = this; ;
+        instance = this;
     }
 
     private void Update()
