80265704a02e13f6909483652d9df15149468ebc
elenanope <elenaromanmoreno@gmail.com>
2025-03-09 22:38:15 +0100

    OnTriggerMenu
    ahora espera el tiempo que tu le digas antes de entrar

--- The Sheperd's Way/Assets/TheShepersWay_Root/Scripts/OnTriggersEvent.cs
+++ The Sheperd's Way/Assets/TheShepersWay_Root/Scripts/OnTriggersEvent.cs
@@ -5,13 +5,30 @@ using UnityEngine.Events;
 
 public class OnTriggersEvent : MonoBehaviour
 {
+    [SerializeField] int secondsToWait = 1;
+    float contadorTiempo;
+    bool isPressed;
+
     public UnityEvent OnTrigger; // Se verá en el inspector
 
+    private void Update()
+    {
+        if(contadorTiempo >= secondsToWait) OnTrigger.Invoke();
 
+        if (isPressed) contadorTiempo += Time.deltaTime;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        OnTrigger.Invoke();
+        contadorTiempo = 0f;
+        isPressed = true;
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        contadorTiempo = 0f;
+        isPressed = false;
+    }
+
 
 }
