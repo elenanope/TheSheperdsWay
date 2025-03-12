7cde0bb34927daa5196f5df6a3952f0527f83fd4
elenanope <elenaromanmoreno@gmail.com>
2025-03-12 22:30:18 +0100

    SheepWandersAround
    hay que solucionar que si colisiona con algo pare/ cambia la ruta y que pueda ir en más direcciones, creo que esto con pathfinding se puede hacer
    
    pathfinding + solo en un territorio (navmesh?) + a cierta distancia de ellas? y que no se acerquen a enemigos

--- The Sheperd's Way/Assets/TheShepersWay_Root/Scripts/SheepAI.cs
+++ The Sheperd's Way/Assets/TheShepersWay_Root/Scripts/SheepAI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class SheepAI : MonoBehaviour
@@ -13,6 +14,12 @@ public class SheepAI : MonoBehaviour
     public Transform objectToFollow;
     [SerializeField] float distanceBetween = 2f;
     FormationLeader leader;
+    int direction = 1; // 1 = derecha, -1 = izquierda
+
+    [SerializeField] float wanderingSpeed = 5;
+    [SerializeField] bool isWandering;
+    [SerializeField] bool isWalking;
+    [SerializeField] bool isFacingRight = true;
 
     //Autoreferences
     BoxCollider2D sheepCol;
@@ -22,6 +29,9 @@ public class SheepAI : MonoBehaviour
     private void OnDisable()
     {
         StopFollowing();
+        StopAllCoroutines();
+        isWandering = false;
+        isWalking = false;
     }
     void Start()
     {
@@ -31,6 +41,28 @@ public class SheepAI : MonoBehaviour
         sheepAnim = GetComponent<Animator>();
         sheepCanDie = true;
     }
+    private void FixedUpdate()
+    {
+        if (!sheepInLine)
+        {
+            if (!isWandering) StartCoroutine(Wander());
+
+            if (isWalking)
+            {
+                sheepRb.velocity = new Vector2(direction * wanderingSpeed, sheepRb.velocity.y);
+            }
+            else
+            {
+                sheepRb.velocity = new Vector2(0, sheepRb.velocity.y);
+            }
+        }
+        else
+        {
+            StopAllCoroutines();
+            isWandering = false;
+            isWalking = false;
+        }
+    }
 
     void Update()
     {
@@ -38,10 +70,17 @@ public class SheepAI : MonoBehaviour
         if (sheepLife <= 0) SheepDeath();
         if (sheepInLine) FollowingDog();
         if (leader.sheepsInLine.Count == 0) StopFollowing();
+
+        
+        if (isWalking) sheepAnim.SetBool("Walk", true);
+        else sheepAnim.SetBool("Walk", false);
+
+        //flipearla segun pa donde vaya
+            
     }
 
     #region Sheep Behaviours
-    void Chill()
+    void Wandering()
     {
         //En NavMesh
     }
@@ -59,9 +98,16 @@ public class SheepAI : MonoBehaviour
 
             if (currentDistance >= distanceBetween)
             {
+                isWalking = true;
                 transform.position = Vector3.MoveTowards(transform.position, objectToFollow.position, sheepSpeed * Time.deltaTime);
+                if (transform.position.x > objectToFollow.position.x && isFacingRight) Flip();
+                else if (transform.position.x < objectToFollow.position.x && !isFacingRight) Flip();
             }
-            else transform.position = transform.position;
+            else 
+            {
+                transform.position = transform.position;
+                isWalking = false;
+            }
         }
     }
 
@@ -88,6 +134,42 @@ public class SheepAI : MonoBehaviour
     }
     #endregion
 
+
+    IEnumerator Wander()
+    {
+        int walkWait = Random.Range(4, 11);
+        int walkTime = Random.Range(0, 3);
+        int flipWait = Random.Range(0, 8);
+        bool flippingASheep = Random.Range(0, 2) == 0;
+
+        isWandering = true;
+
+        yield return new WaitForSeconds(walkWait);
+
+        isWalking = true;
+
+        yield return new WaitForSeconds(walkTime);
+
+        isWalking = false;
+
+        yield return new WaitForSeconds(flipWait);
+
+        if (flippingASheep) Flip();
+
+        isWandering = false;
+
+        //Meter tmb que pueda bajar la cabeza tipo ñam ñam hierba
+
+    }
+
+    void Flip()
+    {
+        direction *= -1;
+        Vector3 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
+        isFacingRight = !isFacingRight;
+    }
     public void SheepTakesDamage(int damage)
     {
         sheepAnim.SetTrigger("Hurt");
