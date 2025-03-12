7cd7dcc388925ab1f8e53bc3ae8b1ec5e8061408
elenanope <elenaromanmoreno@gmail.com>
2025-03-12 02:11:45 +0100

    BuildBasicaYArreglillos

--- The Sheperd's Way/Assets/TheShepersWay_Root/Scripts/SheepAI.cs
+++ The Sheperd's Way/Assets/TheShepersWay_Root/Scripts/SheepAI.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class SheepAI : MonoBehaviour
 {
