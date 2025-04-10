089197ef18890ed86c920728dc805258498a7545
elenanope <elenaromanmoreno@gmail.com>
2025-04-10 18:47:06 +0200

    nuevoFillMainMenu
    ui nueva y cambio de ontriggerenter, ahora hace falta ponerle fill a todos los botones que tengan eso (se cambia la escala en y)

--- The Sheperd's Way/Assets/TheShepersWay_Root/Scripts/OnTriggersEvent.cs
+++ The Sheperd's Way/Assets/TheShepersWay_Root/Scripts/OnTriggersEvent.cs
@@ -7,6 +7,7 @@ public class OnTriggersEvent : MonoBehaviour
 {
     [SerializeField] int secondsToWait = 1;
     float contadorTiempo;
+    [SerializeField] float maxEscalaY = 1.1f;
     bool isPressed;
     [SerializeField] GameObject chargingSymbol;
 
@@ -22,23 +23,23 @@ public class OnTriggersEvent : MonoBehaviour
         }
 
         if (isPressed) contadorTiempo += Time.deltaTime;
+        if (chargingSymbol != null) chargingSymbol.transform.localScale = new Vector3(chargingSymbol.transform.localScale.x, Mathf.Clamp(contadorTiempo / secondsToWait, 0f, maxEscalaY), chargingSymbol.transform.localScale.z);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         contadorTiempo = 0f;
         isPressed = true;
-
-        if (chargingSymbol != null) chargingSymbol.SetActive(false);
-
-        //Opción para botones TSW
-        chargingSymbol.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         contadorTiempo = 0f;
         isPressed = false;
+        if (chargingSymbol != null)
+        {
+            chargingSymbol.transform.localScale = new Vector3(chargingSymbol.transform.localScale.x, 0f, chargingSymbol.transform.localScale.z);
+        }
         //me da error  chargingSymbol.SetActive(false);
     }
 
